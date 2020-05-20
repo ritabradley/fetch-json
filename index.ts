e5f93da1b92d53b7dff0518bf9e6f6cac2b947bb
Rita Bradley <contact@ritabradley.com>
2020-05-20 09:08:51 -0700

    feat: Add type annotations to catch errors

--- index.ts
+++ index.ts
@@ -2,4 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((res) => console.log(res.data));
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+axios.get(url).then((res) => {
+    const todo = res.data as Todo;
+
+    const ID = todo.id;
+    const title = todo.title;
+    const finished = todo.completed;
+
+    const report = `Todo with ID ${ID} has a title of ${title.toUpperCase()}. Is it completed: ${finished}`;
+
+    console.log(report);
+});
